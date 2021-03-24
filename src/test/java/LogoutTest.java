a9c3ef2bc6c2bed9302ca9e7b05092a4e89be4c0
Anna Pogrebinskaya <aniapog@mail.ru>
2021-03-24 21:05:38 +0200

    added test class

---
+++ src/test/java/LogoutTest.java
@@ -0,0 +1,15 @@
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class LogoutTest extends TestBase{
+
+    @BeforeMethod
+    public void preconditions(){
+        //login
+    }
+
+    @Test
+    public void ligoutTest(){
+        //sing out
+    }
+}
